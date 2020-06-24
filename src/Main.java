86e834103f3f079c873e1e133fbefab374f8e964
Daniela Brenes <danibreo6@gmail.com>
2020-06-24 00:18:38 -0600

    Created Binary Tree
    Created the base of the binary tree, with an incomplete insertion method.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,35 @@
 import dataStructures.lists.SinglyList;
 import dataStructures.sorters.Sorter;
+import dataStructures.tree.BinaryTree;
 
 public class Main {
 
     static SinglyList<Integer> numberList = new SinglyList<>();
 
-
     public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>();
 
-        numberList.add(8);
-        numberList.add(2);
-        numberList.add(5);
-        numberList.add(4);
-        numberList.add(3);
-        numberList.add(2);
-        numberList.add(3);
-        numberList.add(4);
-        numberList.print();
-  //      Sorter.insertSort(numberList);
-        Sorter.bubbleSort(numberList);
-        numberList.print();
-        System.out.println("hola");
+//        numberList.add(8);
+//        numberList.add(2);
+//        numberList.add(5);
+//        numberList.add(4);
+//        numberList.add(3);
+//        numberList.add(2);
+//        numberList.add(3);
+//        numberList.add(4);
+//        numberList.print();
+//        Sorter.insertionSort(numberList);
+//        //Sorter.bubbleSort(numberList);
+//        numberList.print();
 
-
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(4);
+        tree.insert(5);
+        tree.insert(6);
+        tree.insert(7);
+        tree.insert(8);
+        tree.insert(9);
     }
-
 }
