5f55b4af9c698920071bcd6f71bde4efc145430a
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-06-25 23:14:51 -0600

    SplayTree implemented
    the base logic needed for a splaytree to work has been implemented to the project.  This implementation is still to be tested

---
+++ src/main/java/com/btp/dataStructures/trees/SplayTree.java
@@ -0,0 +1,283 @@
+package com.btp.dataStructures.trees;
+
+import com.btp.dataStructures.nodes.SplayNode;
+
+/**
+ * the public class for the SplayTree instances. This code is based in the tutorial found in
+ * https://www.sanfoundry.com/java-program-implement-splay-tree/, with major syntax modifications
+ * for using generic type nodes and getter/setter methods for data-scope reduction.
+ * @param <T> generic type of objects to possibly contain in the tree instance
+ */
+public class SplayTree<T extends Comparable<T>> {
+    private SplayNode<T> root;
+    int values = 0;
+
+    public SplayTree() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insert(T element) {
+        SplayNode<T> current = root;
+        SplayNode<T> parent = null;
+
+        while (current != null){
+            parent = current;
+            int x = element.compareTo(parent.getElement());
+            if (x > 0){
+                current = current.getRight();
+            }
+            else{
+                current = current.getLeft();
+            }
+        }
+        current = new SplayNode(0);
+        current.setElement(element);
+        current.setParent(parent);
+        int x = element.compareTo(parent.getElement());
+        if (parent == null) {
+            root = current;
+        }
+        else if (x > 0){
+            parent.setRight(current);
+        }
+        else {
+            parent.setLeft(current);
+        }
+        Splay(current);
+        values++;
+    }
+
+    // rotations
+    public void makeLeftchildParent(SplayNode<T> child, SplayNode<T> parent) {
+        if ((child == null) || (parent == null) || (parent.getLeft() != child) || (child.getParent() != parent)){
+            throw new RuntimeException("wrong rotational operation for this value");
+        }
+
+        if (parent.getParent() != null) {
+            if (parent == parent.getParent().getLeft()){
+                parent.getParent().setLeft(child);
+            }
+            else{
+                parent.getParent().setRight(child);
+            }
+        }
+
+        if (child.getRight() != null){
+            child.getRight().setParent(parent);
+        }
+
+        child.setParent(parent.getParent());
+        parent.setParent(child);
+        parent.setLeft(child.getRight());
+        child.setRight(parent);
+        //zig zag memory go brrr
+        //TODO: actually understand what is going on
+    }
+
+    public void makeRightChildParent(SplayNode<T> child, SplayNode<T> parent) {
+        if ((child == null) || (parent == null) || (parent.getRight() != child) || (child.getParent() != parent)){
+            throw new RuntimeException("wrong rotational operation for this value");
+        }
+        if (parent.getParent() != null) {
+            if (parent == parent.getParent().getLeft()) {
+                parent.getParent().setLeft(child);
+            }
+            else {
+                parent.getParent().setRight(child);
+            }
+        }
+        if (child.getLeft() != null) {
+            child.getLeft().setParent(parent);
+        }
+
+        child.setParent(parent.getParent());
+        parent.setParent(child);
+        parent.setRight(child.getLeft());
+        child.setLeft(parent);
+    }
+
+    //splay function
+    private void Splay(SplayNode<T> x) {
+        while (x.getParent() != null) {
+            SplayNode<T> parent = x.getParent();
+            SplayNode<T> grandParent = parent.getParent();
+            if (grandParent == null) {
+                if (x == parent.getLeft()) {
+                    makeLeftchildParent(x, parent);
+                } else {
+                    makeRightChildParent(x, parent);
+                }
+            } else {
+                if (x == parent.getLeft()) {
+                    if (parent == grandParent.getLeft()) {
+                        makeLeftchildParent(parent, grandParent);
+                        makeLeftchildParent(x, parent);
+                    }
+                    else {
+                        makeLeftchildParent(x, x.getParent());
+                        makeRightChildParent(x, x.getParent());
+                    }
+                }
+                else {
+                    if (parent == grandParent.getLeft()){
+                        makeRightChildParent(x, x.getParent());
+                        makeLeftchildParent(x, x.getParent());
+                    }
+                    else {
+                        makeRightChildParent(parent, grandParent);
+                        makeRightChildParent(x, parent);
+                    }
+                }
+            }
+        }
+        root = x;
+    }
+
+    public void remove(T element) {
+        SplayNode<T> temp = findNode(element);
+        remove(temp);
+    }
+
+    private void remove(SplayNode<T> toRemove) {
+        if (toRemove == null) {
+            return;
+        }
+        Splay(toRemove);
+
+        if ((toRemove.getLeft() != null) && (toRemove.getRight() != null)) {
+            SplayNode<T> min = toRemove.getLeft();
+            while (min.getRight() != null) {
+                min = min.getRight();
+            }
+
+            min.setRight(toRemove.getRight());
+            toRemove.getRight().setParent(min);
+            toRemove.getLeft().setParent(null);
+            root = toRemove.getLeft();
+        }
+        else if (toRemove.getRight() != null) {
+            toRemove.getRight().setParent(null);
+            root = toRemove.getRight();
+        }
+        else if (toRemove.getLeft() != null) {
+            toRemove.getLeft().setParent(null);
+            root = toRemove.getLeft();
+        }
+        else {
+            root = null;
+        }
+
+        toRemove.setParent(null);
+        toRemove.setLeft(null);
+        toRemove.setRight(null);
+        toRemove = null;
+        values--;
+    }
+
+    /**
+     * method to get the amount of nodes in the tree
+     * @return integer value for amount of nodes.
+     */
+    public int countNodes() {
+        return values;
+    }
+
+    /**
+     * method to search for an element in the tree
+     * @param element the value that is to be found
+     * @return boolean value, true if the value is in tree, false otherwise.
+     */
+    public boolean contains(T element){
+        return findNode(element) != null;
+    }
+
+    /**
+     * private method to find a node with the element it contains
+     * @param element the value contained by the node to be found, when the search method invokes this method
+     * @return a node, if the value exists in the tree, null if it was not found
+     */
+    private SplayNode<T> findNode(T element) {
+        SplayNode<T> previous = null;
+        SplayNode<T> current = root;
+        while (current != null) {
+            previous = current;
+            int x = element.compareTo(current.getElement());
+            if (x > 0) {
+                current = current.getRight();
+            }
+            else if (x < 0) {
+                current = current.getLeft();
+            }
+            else {
+                Splay(current);
+                return current;
+            }
+        }
+        if (previous != null) {
+            Splay(previous);
+            return null;
+        }
+        return null;
+    }
+
+    /**
+     * in order traversal method
+     */
+    public void inOrder() {
+        inorder(this.root);
+    }
+
+    /**
+     * private inorder traversal method
+     * @param root the root object of the SplayTree instance
+     */
+    private void inorder(SplayNode<T> root) {
+        if (root != null) {
+            inorder(root.getLeft());
+            System.out.println(root.getElement() + " ");
+            inorder(root.getRight());
+        }
+    }
+
+    /**
+     * public preorder traversal method
+     */
+    public void preorder() {
+        preorder(this.root);
+    }
+
+    /**
+     * private preorder traversal method
+     * @param root root object of the SplayTree instance
+     */
+    private void preorder(SplayNode<T> root) {
+        if (root != null) {
+            System.out.println(root.getElement() + " ");
+            preorder(root.getLeft());
+            preorder(root.getRight());
+        }
+    }
+
+    /**
+     * public postorder traversal method
+     */
+    public void postorder() {
+        postorder(this.root);
+    }
+
+    /**
+     * private preorder traversal method
+     * @param root the root of the SplayTree Instance
+     */
+    private void postorder(SplayNode<T> root) {
+        if (root != null) {
+            postorder(root.getLeft());
+            postorder(root.getRight());
+            System.out.println(root.getElement() + " ");
+        }
+    }
+}
