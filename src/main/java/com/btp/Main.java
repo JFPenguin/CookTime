36bd28c8157c6a09ee45d1d147b45651dc76604d
JoeA42 <joe.alvarez42@gmail.com>
2020-06-27 02:14:08 -0600

    [changed Temp Resources] Removed test resources from Resources.java and added them to Main.java init() method, so it runs once on server start up

--- src/main/java/com/btp/Main.java
+++ src/main/java/com/btp/Main.java
@@ -1,6 +1,8 @@
 package com.btp;
 
-import javax.servlet.ServletException;
+import com.btp.dataStructures.lists.SinglyList;
+import com.btp.serverData.*;
+
 import javax.servlet.http.HttpServlet;
 import javax.swing.*;
 import java.awt.*;
@@ -9,10 +11,11 @@ import java.awt.*;
 public class Main extends HttpServlet {
 
     public void init() {
+        testResources();
         createWindow();
     }
 
-    private static void createWindow(){
+    private static void createWindow() {
         JFrame frame = new JFrame("simpleGUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel textLabel = new JLabel("Hola soy su server pa",SwingConstants.CENTER);
@@ -22,5 +25,80 @@ public class Main extends HttpServlet {
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
+
+    }
+
+    public void testResources() {
+
+        //Test Users
+        User u1 = new User();
+        u1.setName("Pedrito Johnson"); u1.setEmail("xxxxXXXXpedritoSexyGamer420XXXxxx@yahoo.com");u1.setPassword("password");u1.setAge(13);
+
+        User u2 = new User();
+        u2.setName("Ojo Noda"); u2.setEmail("OjoNoda@gmail.com");u2.setPassword("password");u2.setAge(18);
+
+        User u3 = new User();
+        u3.setName("Fus RoDah"); u3.setEmail("dragonborn69@bugtesda.com");u3.setPassword("password");u3.setAge(18);
+
+        User u4 = new User();
+        u4.setName("Michael Jayson Toshiba"); u4.setEmail("theRealMichaelJayson@gmail.com");u4.setPassword("password");u4.setAge(65);
+
+        User u5 = new User();
+        u5.setName("Kenny Velasquez"); u5.setEmail("kennyBellius@gmail.com");u5.setPassword("password");u5.setAge(22);
+
+
+        UserRepo.addUser(u1);
+        UserRepo.addUser(u2);
+        UserRepo.addUser(u3);
+        UserRepo.addUser(u4);
+        UserRepo.addUser(u5);
+
+
+        //Test Recipes
+        //Gallo Pinto
+        SinglyList<DishTag> galloPintoTags = new SinglyList<>();
+        galloPintoTags.add(DishTag.VEGAN);
+
+        SinglyList<Ingredient> galloPintoIngredients = new SinglyList<>();
+        galloPintoIngredients.add(new Ingredient("Arroz",1, MeasurementUnit.CUP));
+        galloPintoIngredients.add(new Ingredient("Frijoles",1,MeasurementUnit.CUP));
+        galloPintoIngredients.add(new Ingredient("Especies Mixtas",(float)1/2,MeasurementUnit.CUP));
+        galloPintoIngredients.add(new Ingredient("Lizano, obvio",(float) 1/2, MeasurementUnit.CUP));
+
+        SinglyList<String> galloPintoInstructions = new SinglyList<>();
+        galloPintoInstructions.add("Revolver las varas");
+        galloPintoInstructions.add("Cocinar");
+
+        Recipe galloPinto = new Recipe("Gallo Pinto",UserRepo.getUser(0), DishTime.BREAKFAST,
+                5,15, DishType.MAIN_DISH,3,galloPintoTags,galloPintoIngredients,galloPintoInstructions);
+
+        //CheeseCake
+        SinglyList<DishTag> CheeseCakeTags = new SinglyList<>();
+        CheeseCakeTags.add(DishTag.VEGETARIAN);
+
+        SinglyList<Ingredient> cheeseCakeIngredients = new SinglyList<>();
+        cheeseCakeIngredients.add(new Ingredient("Queso Crema",300,MeasurementUnit.GRAM));
+        cheeseCakeIngredients.add(new Ingredient("Azucar",1,MeasurementUnit.CUP));
+        cheeseCakeIngredients.add(new Ingredient("Galleta Maria",500,MeasurementUnit.GRAM));
+        cheeseCakeIngredients.add(new Ingredient("Mantequilla",150,MeasurementUnit.GRAM));
+        cheeseCakeIngredients.add(new Ingredient("Huevos", 3,MeasurementUnit.UNIT));
+        cheeseCakeIngredients.add(new Ingredient("Natilla",1,MeasurementUnit.CUP));
+        cheeseCakeIngredients.add(new Ingredient("Jalea de Fresa",(float)1/2,MeasurementUnit.CUP));
+
+        SinglyList<String> cheeseCakeInstructions = new SinglyList<>();
+        cheeseCakeInstructions.add("Precalentar horno a 350°F ");
+        cheeseCakeInstructions.add("Pulverize la galleta maria y mézclela con la mantequilla" +
+                " y recubra el interior de un molde para queques");
+        cheeseCakeInstructions.add("Mezcle el queso crema y la azucar hasta que tenga una consistencia suave, " +
+                "agregue los huevos uno a uno, mezclando despues de cada uno");
+        cheeseCakeInstructions.add("Hornee en el horno precalentado por 50 minutos, espere a que se enfrie por completo antes de remover del molde");
+
+        Recipe cheeseCake = new Recipe("Cheese Cake",UserRepo.getUser(1), DishTime.BREAKFAST,
+                5,15, DishType.MAIN_DISH,3,CheeseCakeTags,cheeseCakeIngredients,cheeseCakeInstructions);
+
+        RecipeRepo.addRecipe(cheeseCake);
+        RecipeRepo.addRecipe(galloPinto);
+
+
     }
 }
--- src/main/java/com/btp/Resources.java
+++ src/main/java/com/btp/Resources.java
@@ -1,9 +1,7 @@
 package com.btp;
 
-import com.btp.serverData.Recipe;
-import com.btp.serverData.RecipeRepo;
-import com.btp.serverData.User;
-import com.btp.serverData.UserRepo;
+import com.btp.dataStructures.lists.SinglyList;
+import com.btp.serverData.*;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
@@ -12,81 +10,6 @@ import jakarta.ws.rs.core.MediaType;
  */
 @Path("resources")
 public class Resources {
-
-
-//    public Resources() {
-//
-//        //Test Users
-//        User u1 = new User();
-//        u1.setName("Pedrito Johnson"); u1.setEmail("xxxxXXXXpedritoSexyGamer420XXXxxx@yahoo.com");u1.setPassword("password");u1.setAge(13);
-//
-//        User u2 = new User();
-//        u2.setName("Ojo Noda"); u2.setEmail("OjoNoda@gmail.com");u2.setPassword("password");u2.setAge(18);
-//
-//        User u3 = new User();
-//        u3.setName("Fus RoDah"); u3.setEmail("dragonborn69@bugtesda.com");u3.setPassword("password");u3.setAge(18);
-//
-//        User u4 = new User();
-//        u4.setName("Michael Jayson Toshiba"); u4.setEmail("theRealMichaelJayson@gmail.com");u4.setPassword("password");u4.setAge(65);
-//
-//        User u5 = new User();
-//        u5.setName("Kenny Velasquez"); u5.setEmail("kennyBellius@gmail.com");u5.setPassword("password");u5.setAge(22);
-//
-//
-//        userRepo.addUser(u1);
-//        userRepo.addUser(u2);
-//        userRepo.addUser(u3);
-//        userRepo.addUser(u4);
-//        userRepo.addUser(u5);
-//
-//
-//        //Test Recipes
-//        //Gallo Pinto
-//        SinglyList<DishTag> galloPintoTags = new SinglyList<>();
-//        galloPintoTags.add(DishTag.VEGAN);
-//
-//        SinglyList<Ingredient> galloPintoIngredients = new SinglyList<>();
-//        galloPintoIngredients.add(new Ingredient("Arroz",1,MeasurementUnit.CUP));
-//        galloPintoIngredients.add(new Ingredient("Frijoles",1,MeasurementUnit.CUP));
-//        galloPintoIngredients.add(new Ingredient("Especies Mixtas",1/2,MeasurementUnit.CUP));
-//        galloPintoIngredients.add(new Ingredient("Lizano, obvio",1/2,MeasurementUnit.CUP));
-//
-//        SinglyList<String> galloPintoInstructions = new SinglyList<>();
-//        galloPintoInstructions.add("Revolver las varas");
-//        galloPintoInstructions.add("Cocinar");
-//
-//        Recipe galloPinto = new Recipe("Gallo Pinto",userRepo.getUser(0), DishTime.BREAKFAST,
-//                5,15, DishType.MAIN_DISH,3,galloPintoTags,galloPintoIngredients,galloPintoInstructions);
-//
-//        //CheeseCake
-//        SinglyList<DishTag> CheeseCakeTags = new SinglyList<>();
-//        CheeseCakeTags.add(DishTag.VEGETARIAN);
-//
-//        SinglyList<Ingredient> cheeseCakeIngredients = new SinglyList<>();
-//        cheeseCakeIngredients.add(new Ingredient("Queso Crema",300,MeasurementUnit.GRAM));
-//        cheeseCakeIngredients.add(new Ingredient("Azucar",1,MeasurementUnit.CUP));
-//        cheeseCakeIngredients.add(new Ingredient("Galleta Maria",500,MeasurementUnit.GRAM));
-//        cheeseCakeIngredients.add(new Ingredient("Mantequilla",150,MeasurementUnit.GRAM));
-//        cheeseCakeIngredients.add(new Ingredient("Huevos", 3,MeasurementUnit.UNIT));
-//        cheeseCakeIngredients.add(new Ingredient("Natilla",1,MeasurementUnit.CUP));
-//        cheeseCakeIngredients.add(new Ingredient("Jalea de Fresa",(float)1/2,MeasurementUnit.CUP));
-//
-//        SinglyList<String> cheeseCakeInstructions = new SinglyList<>();
-//        cheeseCakeInstructions.add("Precalentar horno a 350°F ");
-//        cheeseCakeInstructions.add("Pulverize la galleta maria y mézclela con la mantequilla" +
-//                " y recubra el interior de un molde para queques");
-//        cheeseCakeInstructions.add("Mezcle el queso crema y la azucar hasta que tenga una consistencia suave, " +
-//                "agregue los huevos uno a uno, mezclando despues de cada uno");
-//        cheeseCakeInstructions.add("Hornee en el horno precalentado por 50 minutos, espere a que se enfrie por completo antes de remover del molde");
-//
-//        Recipe cheeseCake = new Recipe("Cheese Cake",userRepo.getUser(1), DishTime.BREAKFAST,
-//                5,15, DishType.MAIN_DISH,3,CheeseCakeTags,cheeseCakeIngredients,cheeseCakeInstructions);
-//
-//        recipeRepo.addRecipe(cheeseCake);
-//        recipeRepo.addRecipe(galloPinto);
-//
-//
-//    }
 
 
     /**
