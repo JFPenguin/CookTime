7f3bda0d55d7aa7f28814a37f6f1e5af22094f78
JoeA42 <joe.alvarez42@gmail.com>
2020-06-26 19:57:30 -0600

    [Added server GUI] Added a very simple server GUI

--- src/main/java/com/btp/Main.java
+++ src/main/java/com/btp/Main.java
@@ -1,26 +1,26 @@
 package com.btp;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.swing.*;
 import java.awt.*;
 
-public class Main{
 
+public class Main extends HttpServlet {
+
+    public void init() {
+        createWindow();
+    }
 
     private static void createWindow(){
         JFrame frame = new JFrame("simpleGUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel textLabel = new JLabel("I'm a label",SwingConstants.CENTER);
+        JLabel textLabel = new JLabel("Hola soy su server pa",SwingConstants.CENTER);
         textLabel.setPreferredSize(new Dimension(300,100));
         frame.getContentPane().add(textLabel, BorderLayout.CENTER);
 
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
-    }
-
-
-    public static void main(String[] args) {
-        createWindow();
-        System.out.println("window running");
     }
 }
