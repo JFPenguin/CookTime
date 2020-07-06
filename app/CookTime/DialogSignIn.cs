8e673ec78069b2b34f2f5b6d259fe99123c7b8ba
Daniela Brenes <danibreo6@gmail.com>
2020-07-05 21:14:08 -0600

    Initial structure for mobile app
    Implemented the first window seen when the app launches, and the sign in and sign up views

---
+++ app/CookTime/DialogSignIn.cs
@@ -0,0 +1,21 @@
+﻿using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+
+namespace CookTime {
+    public class DialogSignIn : DialogFragment {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.DialogSignIn, container, false);
+            return view;
+        }
+        
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.fragment_anim;
+        }
+    }
+}
