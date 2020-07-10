7c6bb8dc75b017e1f1a7945fa6f309055729371a
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-07-09 21:33:08 -0600

    testing changes
    created new users for testing purposes. Restored IP to previous value, and fixed an error in the main CS file for the application.

--- app/CookTime/DialogSignIn.cs
+++ app/CookTime/DialogSignIn.cs
@@ -57,7 +57,7 @@ namespace CookTime {
             else {
                 using var webClient = new WebClient {BaseAddress = "http://" + MainActivity.Ipv4 + ":8080/CookTime_war/cookAPI/"};
 
-                var url = "resources/auth?email=" + userEmailInput;
+                var url = "resources/auth?email=" + userEmailInput + "&password=" + userPasswordInput;
                 webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
                 var send = webClient.DownloadString(url);
 
