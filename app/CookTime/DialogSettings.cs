d50785cc165be4ee8a6e95deceb1f34536d3b0f5
Daniela Brenes <danibreo6@gmail.com>
2020-07-14 17:59:19 -0600

    User-info-edit
    Added GUI to change the user's password

--- app/CookTime/DialogSettings.cs
+++ app/CookTime/DialogSettings.cs
@@ -8,14 +8,16 @@ using Newtonsoft.Json;
 
 namespace CookTime {
     /// <summary>
-    /// This class represents the dialog fragment that shows when the user wants to sign in
+    /// This class represents the dialog fragment that shows when the user wants to edit its info
     /// </summary>
-    public class DialogSignIn : DialogFragment {
-        private EditText _userEmail;
-        private EditText _userPassword;
-        private Button _btnSendSignIn;
-        public event EventHandler<SignInEvent> EventHandlerSignIn;
-        
+    public class DialogSettings : DialogFragment {
+        private string _email;
+        private EditText _currentPass;
+        private EditText _newPass;
+        private EditText _newPass2;
+        private Button _btnPass;
+        public event EventHandler<SendPassEvent> EventHandlerPass;
+
         /// <summary>
         /// Creates the fragment, instantiates its user interface view and returns the view
         /// </summary>
@@ -27,50 +29,55 @@ namespace CookTime {
         {
             base.OnCreateView(inflater, container, savedInstanceState);
 
-            var view = inflater.Inflate(Resource.Layout.DialogSignIn, container, false);
+            var view = inflater.Inflate(Resource.Layout.DialogSettings, container, false);
             
-            _userEmail = view.FindViewById<EditText>(Resource.Id.signInEmail);
-            _userPassword = view.FindViewById<EditText>(Resource.Id.signInPassword);
-            _btnSendSignIn = view.FindViewById<Button>(Resource.Id.btnSendSignIn);
+            _currentPass = view.FindViewById<EditText>(Resource.Id.currentPass);
+            _newPass = view.FindViewById<EditText>(Resource.Id.newPass);
+            _newPass2 = view.FindViewById<EditText>(Resource.Id.newPass2);
+            _btnPass = view.FindViewById<Button>(Resource.Id.btnPassword);
             
-            _btnSendSignIn.Click += SendSignIn;
+            _btnPass.Click += SendPassword;
             
             return view;
         }
         
         /// <summary>
-        /// This method is called when the user presses the button to sign up.
-        /// It invokes the SignUpEvent event, instantiating the class and passing all the data.
+        /// This method is called when the user presses the button to submit the changes.
+        /// It invokes the SendPassword event, instantiating the class and passing all the data.
         /// Finally it closes the fragment.
         /// </summary>
         /// <param name="sender"> Reference to the object that raised the event</param>
         /// <param name="e"> Contains the event data </param>
-        private void SendSignIn(object sender, EventArgs e) {
-            var userEmailInput = _userEmail.Text;
-            var userPasswordInput = _userPassword.Text;
+        private void SendPassword(object sender, EventArgs e) {
+            var currentPassInput = _currentPass.Text;
+            var newPassInput = _newPass.Text;
+            var newPassInput2 = _newPass2.Text;
             string value;
 
-            if (userEmailInput.Equals("") || userPasswordInput.Equals("")) {
+            if (currentPassInput.Equals("") || newPassInput.Equals("") || newPassInput2.Equals("")) {
                 value = "3";
             }
             
+            else if (!newPassInput.Equals(newPassInput2)) {
+                value = "2";
+            }
             else {
                 using var webClient = new WebClient {BaseAddress = "http://" + MainActivity.Ipv4 + ":8080/CookTime_war/cookAPI/"};
-                var url = "resources/auth?email=" + userEmailInput + "&password=" + userPasswordInput;
+                var url = "resources/editUserPassword?email=" + _email + "&newPassword=" + newPassInput + "&password=" + currentPassInput;
                 webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
                 var send = webClient.DownloadString(url);
                 
                 var response = JsonConvert.DeserializeObject<string>(send);
                     
                 value = response;
-                
+
                 if (value == "1") {
                     Dismiss();
                 }
             }            
             
-            if (EventHandlerSignIn != null)
-                EventHandlerSignIn.Invoke(this, new SignInEvent(userEmailInput, value));
+            if (EventHandlerPass != null)
+                EventHandlerPass.Invoke(this, new SendPassEvent(value));
         }
         
         /// <summary>
@@ -82,20 +89,26 @@ namespace CookTime {
             base.OnActivityCreated(savedInstanceState);
             Dialog.Window.Attributes.WindowAnimations = Resource.Style.fragment_anim;
         }
+
+        /// <summary>
+        /// Property for the _email attribute
+        /// </summary>
+        public string Email
+        {
+            set => _email = value;
+        }
     }
     
     /// <summary>
     /// This class represents an event. It contains all of the data that the user entered.
     /// The properties inside this class will let the main view access the user data.
     /// </summary>
-    public class SignInEvent : EventArgs {
+    public class SendPassEvent : EventArgs {
         /// <summary>
         /// Constructor for the SignInEvent class
         /// </summary>
-        /// <param name="userEmail"> The email input from the user  </param>
         /// <param name="message"> String that will indicate the text in a message for the user  </param>
-        public SignInEvent(string userEmail, string message) {
-            UserEmail = userEmail;
+        public SendPassEvent(string message) {
             Message = message;
         }
         
@@ -103,11 +116,5 @@ namespace CookTime {
         /// Property for the message attribute
         /// </summary>
         public string Message { get; }
-
-        /// <summary>
-        /// Property for the userEmail attribute
-        /// </summary>
-        public string UserEmail { get;}
     }
-    
 }
