5323b72280ee26d7b33e7260859ed3343574a41b
Daniela Brenes <danibreo6@gmail.com>
2020-07-16 12:16:00 -0600

    See-other-profiles
    Seeing other user's profile is now possible

--- app/CookTime/FollowActivity.cs
+++ app/CookTime/FollowActivity.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Widget;
@@ -11,13 +13,14 @@ namespace CookTime {
     /// </summary>
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
     public class FollowActivity : AppCompatActivity {
+        private string _loggedId;
         private TextView _titleText;
         private ListView _followListView;
         private IList<string> followList;
         
         /// <summary>
         /// This method is called when the activity is starting.
-        /// It contains the logic for the buttons shown in the first view.
+        /// The list of followers/following is displayed here.
         /// </summary>
         /// <param name="savedInstanceState"> a Bundle that contains the data the activity most recently
         /// supplied if the activity is being re-initialized after previously being shut down. </param>
@@ -32,11 +35,46 @@ namespace CookTime {
             var title = Intent.GetStringExtra("Title");
             _titleText.Text = title;
 
+            _loggedId = Intent.GetStringExtra("LoggedId");
+
             followList = Intent.GetStringArrayListExtra("FollowList");
             
             FollowAdapter adapter = new FollowAdapter(this, followList);
             
             _followListView.Adapter = adapter;
+            
+            _followListView.ItemClick += FollowClick;
+        }
+
+        private void FollowClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            string id = followList[e.Position].Split(";")[0];
+
+            if (id == _loggedId) {
+                using var webClient = new WebClient {BaseAddress = "http://" + MainActivity.Ipv4 + ":8080/CookTime_war/cookAPI/"};
+
+                var url = "resources/getUser?id=" + id;
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var send = webClient.DownloadString(url);
+                
+                Intent intent = new Intent(this, typeof(MyProfileActivity));
+                intent.PutExtra("User", send);
+                StartActivity(intent);
+                OverridePendingTransition(Android.Resource.Animation.SlideInLeft,Android.Resource.Animation.SlideOutRight);
+            }
+            else {
+                using var webClient = new WebClient {BaseAddress = "http://" + MainActivity.Ipv4 + ":8080/CookTime_war/cookAPI/"};
+
+                var url = "resources/getUser?id=" + id;
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var send = webClient.DownloadString(url);
+                
+                Intent intent = new Intent(this, typeof(PrivProfileActivity));
+                intent.PutExtra("User", send);
+                intent.PutExtra("LoggedId", _loggedId);
+                StartActivity(intent);
+                OverridePendingTransition(Android.Resource.Animation.SlideInLeft,Android.Resource.Animation.SlideOutRight); 
+            }
         }
     }
 }
