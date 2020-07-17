f8836322fc0e4d4e1f85a8d8f97c26acbad8aab0
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-07-16 22:16:34 -0600

    newsfeed-profile-connection-ready
    Newsfeed now loads the newsfeed list properly from server requests, and switches to profile view with a window, all tested, up and running.

--- app/CookTime/Activities/NewsfeedActivity.cs
+++ app/CookTime/Activities/NewsfeedActivity.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using Android.App;
-using Android.App.Usage;
 using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Widget;
+using Java.Util;
 using Newtonsoft.Json;
 
 namespace CookTime.Activities {
@@ -16,8 +17,6 @@ namespace CookTime.Activities {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
     public class NewsfeedActivity : AppCompatActivity {
         private User _loggedUser;
-        private List<string> _followedMails;
-        private List<User> _followed;
         private Toast _toast;
         private Button _profileButton;
         
@@ -30,36 +29,38 @@ namespace CookTime.Activities {
         /// supplied if the activity is being re-initialized after previously being shut down. </param>
         protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
+            
             SetContentView(Resource.Layout.Newsfeed);
             
-            string json = Intent.GetStringExtra("User");
+            var json = Intent.GetStringExtra("User");
             _loggedUser = JsonConvert.DeserializeObject<User>(json);
-
+            
+            _profileButton = FindViewById<Button>(Resource.Id.profileButton);
+            _profileButton.Click += ProfileClick;
+            
             using var webClient = new WebClient {BaseAddress = "http://" + MainActivity.Ipv4 + ":8080/CookTime_war/cookAPI/"};
-            var url = "resources/newsfeed?id=" + _loggedUser.email;
+            var url = "resources/newsfeed?email=" + _loggedUser.email;
             webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
             var request = webClient.DownloadString(url);
-            var response = JsonConvert.DeserializeObject<string>(request);
-            //TODO solve request-response relation to properly get the newsfeed from server
+            var response = JsonConvert.DeserializeObject<List<string>>(request);
         }
 
-        private void ProfileClick(object sender, UsageEvents.Event e)
+        private void ProfileClick(object sender, EventArgs e)
         {
             string toastText = "opening MyProfile...";
             _toast = Toast.MakeText(this, toastText, ToastLength.Short);
             _toast.Show();
             
-            using var webClient = new WebClient {BaseAddress = "http://" + MainActivity.Ipv4 + ":8080/CookTime_war/cookAPI/"};
+             // converting the existing user to a json string
+             var send = JsonConvert.SerializeObject(_loggedUser);
 
-            var url = "resources/getUser?id=" + _loggedUser.email;
-            webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
-            var send = webClient.DownloadString(url);
             Intent profileIntent = new Intent(this, typeof(MyProfileActivity));
-            Intent.PutExtra("User", send);
+            // passing the serialized User object as an intent extra with json string format
+            profileIntent.PutExtra("User", send);
+
             StartActivity(profileIntent);
             OverridePendingTransition(Android.Resource.Animation.SlideInLeft, Android.Resource.Animation.SlideOutRight);
             Finish();
-            
         }
     }
 }
